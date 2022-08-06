e6dc92c2bc6a058ff80cdca815494541a3e19b82
Dinar <gizzyatovdinar@gmail.com>
2022-08-07 00:28:04 +0400

    changed the Firsttest

--- src/test/java/guru_qa/FirstTest.java
+++ src/test/java/guru_qa/FirstTest.java
@@ -1,17 +1,17 @@
-//package guru_qa;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class FirstTest {
-//
-//    @Test
-//    void firstTest() {
-//        Assertions.assertTrue(3>2);
-//    }
-//
-//    @Test
-//    void secondTest() {
-//        Assertions.assertTrue(7>6);
-//    }
-//}
+package guru_qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        Assertions.assertFalse(3<2);
+    }
+
+    @Test
+    void secondTest() {
+        Assertions.assertFalse(7<6);
+    }
+}
